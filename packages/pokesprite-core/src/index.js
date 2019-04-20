7553784d553cb2f6da58467c0d88b4b66d7e3ef8
Michiel Sikma <michiel@sikma.org>
2019-04-20 23:52:31 +0200

    Add example of settings format

--- packages/pokesprite-core/src/index.js
+++ packages/pokesprite-core/src/index.js
@@ -1,8 +1,35 @@
 /** PokéSprite - pokesprite-core <https://github.com/msikma/pokesprite>
   * © MIT license */
 
-const generateSprite = async (args) => {
-  console.log('core args', args);
+/**
+ * Generates a sprite.
+ *
+ * Settings format:
+ *
+ *    { icons:
+ *       { pokemon:
+ *          { include: true,
+ *            forms: true,
+ *            regular: true,
+ *            shiny: true,
+ *            genders: 'both',
+ *            flipped: true,
+ *            generateFlipped: false },
+ *         items: { include: true, sets: '*' },
+ *         others: { include: true } },
+ *      minify: true,
+ *      padding: true,
+ *      generate:
+ *       { css: 'pkmn.css',
+ *         html: 'overview.html',
+ *         img: 'pkmn.png',
+ *         md: 'overview.md',
+ *         scss: 'pkmn.scss' } }
+ *
+ * TBA.
+ */
+const generateSprite = async (outputPath, settings) => {
+  console.log('core args', outputPath, settings);
 }
 
 export default {
