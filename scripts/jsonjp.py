3ae1cdab463174bdfd83988d83122bc24d0b3bd6
Michiel Sikma <michiel@sikma.org>
2020-02-17 00:04:54 +0100

    Remove old scripts, add a few helper scripts in Bash for compatibility

--- scripts/jsonjp.py
+++ scripts/jsonjp.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-# prints JSON-usable japanese
-# example: $ ./jsonjp.py モクロー
-# output: "\u30e2\u30af\u30ed\u30fc"
+# Prints input with non-ASCII characters converted to Unicode escape sequences.
+# Example:
+#   $ ./jsonjp.py モクロー
+#   "\u30e2\u30af\u30ed\u30fc"
+
 import json
 import sys
+if len(sys.argv) <= 1:
+  print('usage: jsonjp.py TEXT')
+  sys.exit(1)
 print(json.dumps(sys.argv[1]))
